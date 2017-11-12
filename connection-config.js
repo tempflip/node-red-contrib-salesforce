4969ee86bd59da605a6e198b9d38f5f7549b04a7
Stephan H. Wissel <stw@linux.com>
2017-11-13 00:53:11 +0800

    Added environment auth and pot URL

--- connection-config.js
+++ connection-config.js
@@ -1,12 +1,21 @@
 module.exports = function(RED) {
     function ConnectionConfig(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.consumerKey = n.consumerKey;
         this.consumerSecret = n.consumerSecret;
         this.callbackUrl = n.callbackUrl;
         this.environment = n.environment;
-        this.username = n.username;
-        this.password = n.password;
+        this.poturl = n.poturl;
+        // Optional reading of username/password from environment
+        if (n.useEnvCredentials) {
+            const envUser = n.name + '_UserName';
+            const envPassword = n.name + '_PassWord';
+            this.username = process.env[envUser];
+            this.password = process.env[envPassword];
+        } else {
+            this.username = n.username;
+            this.password = n.password;
+        }
     }
-    RED.nodes.registerType("connection-config",ConnectionConfig);
-}
+    RED.nodes.registerType('connection-config', ConnectionConfig);
+};
--- soql.js
+++ soql.js
@@ -1,46 +1,55 @@
 module.exports = function(RED) {
+    var nforce = require('nforce');
 
-  var nforce = require('nforce');
+    function Query(config) {
+        RED.nodes.createNode(this, config);
+        this.connection = RED.nodes.getNode(config.connection);
+        var node = this;
+        this.on('input', function(msg) {
+            // show initial status of progress
+            node.status({ fill: 'green', shape: 'ring', text: 'connecting....' });
 
-  function Query(config) {
-    RED.nodes.createNode(this,config);
-    this.connection = RED.nodes.getNode(config.connection);
-    var node = this;
-    this.on('input', function(msg) {
+            // use msg query if node's query is blank
+            if (msg.hasOwnProperty('query') && config.query === '') {
+                config.query = msg.query;
+            }
 
-      // show initial status of progress
-      node.status({fill:"green",shape:"ring",text:"connecting...."});
+            // create connection object
+            const orgOptions = {
+                clientId: this.connection.consumerKey,
+                clientSecret: this.connection.consumerSecret,
+                redirectUri: this.connection.callbackUrl,
+                environment: this.connection.environment,
+                mode: 'single'
+            };
 
-      // use msg query if node's query is blank
-      if (msg.hasOwnProperty("query") && config.query === '') {
-        config.query = msg.query;
-      }
+            // Overwrite the endpoints eventually
+            if (this.connection.poturl) {
+                orgOptions.authEndpoint = this.connection.poturl;
+                orgOptions.testAuthEndpoint = this.connection.poturl;
+            }
 
-      // create connection object
-      var org = nforce.createConnection({
-        clientId: this.connection.consumerKey,
-        clientSecret: this.connection.consumerSecret,
-        redirectUri: this.connection.callbackUrl,
-        environment: this.connection.environment,
-        mode: 'single'
-      });
+            const org = nforce.createConnection(orgOptions);
 
-      // auth and run query
-      org.authenticate({ username: this.connection.username, password: this.connection.password }).then(function(){
-        return org.query({ query: config.query })
-      }).then(function(results) {
-        msg.payload = {
-          size: results.totalSize,
-          records: results.records
-        }
-        node.send(msg);
-        node.status({});
-      }).error(function(err) {
-        node.status({fill:"red",shape:"dot",text:"Error!"});
-        node.error(err);
-      });
-
-    });
-  }
-  RED.nodes.registerType("soql",Query);
-}
+            // auth and run query
+            org
+                .authenticate({ username: this.connection.username, password: this.connection.password })
+                .then(function() {
+                    return org.query({ query: config.query });
+                })
+                .then(function(results) {
+                    msg.payload = {
+                        size: results.totalSize,
+                        records: results.records
+                    };
+                    node.send(msg);
+                    node.status({});
+                })
+                .error(function(err) {
+                    node.status({ fill: 'red', shape: 'dot', text: 'Error!' });
+                    node.error(err);
+                });
+        });
+    }
+    RED.nodes.registerType('soql', Query);
+};
