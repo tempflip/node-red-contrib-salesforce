757a3e296e80c5e46a3200416a99d798d889899f
Stephan H. Wissel <stw@linux.com>
2019-03-25 11:51:56 +0800

    Fixed nforce call

--- dml.js
+++ dml.js
@@ -1,69 +1,66 @@
-module.exports = function (RED) {
+module.exports = function(RED) {
+  const nforce = require('./nforce_wrapper');
 
-    const nforce = require('./nforce8_wrapper');
+  function Dml(config) {
+    RED.nodes.createNode(this, config);
+    this.connection = RED.nodes.getNode(config.connection);
+    const node = this;
+    this.on('input', (msg) => {
+      // show initial status of progress
+      node.status({ fill: 'green', shape: 'ring', text: 'connecting....' });
 
-    function Dml(config) {
-        RED.nodes.createNode(this, config);
-        this.connection = RED.nodes.getNode(config.connection);
-        const node = this;
-        this.on('input', msg => {
+      // check for overriding message properties
+      const theAction = msg.hasOwnProperty('action') && config.action === '' ? msg.action : config.action;
+      const theObject = msg.hasOwnPropety('object') && config.object === '' ? msg.object : config.object;
 
-            // show initial status of progress
-            node.status({ fill: "green", shape: "ring", text: "connecting...." });
+      // create connection object
+      const orgResult = nforce.createConnection(this.connection);
+      const sobj = nforce.force.createSObject(theObject, msg.payload);
+      const payload = { sobject: sobj };
+      nforce.extractHeaders(payload, msg);
 
-            // check for overriding message properties
-            const theAction = (msg.hasOwnProperty("action") && config.action === '') ? msg.action : config.action;
-            const theObject = (msg.hasOwnPropety("object") && config.object === '') ? msg.object : config.object;
+      // Auth and run DML
+      nforce
+        .authenticate(orgResult.org, orgResult.config)
+        .then((oauth) => {
+          let dmlResult;
+          const org = orgResult.org;
+          switch (theAction) {
+            case 'insert':
+              dmlResult = org.insert(payload);
+              break;
+            case 'update':
+              dmlResult = org.update(payload);
+              break;
+            case 'upsert':
+              if (msg.hasOwnProperty('externalId')) {
+                sobj.sobject.setExternalId(msg.externalId.field, msg.externalId.value);
+              }
+              dmlResult = org.upsert(payload);
+              break;
+            case 'delete':
+              dmlResult = org.delete(payload);
+              break;
+            default:
+              const err = new Error('Unknown method:' + theAction);
+              nforce.error(node, msg, err);
+          }
 
-            // create connection object
-            const orgResult = nforce.createConnection(this.connection);
-            const sobj = nforce.force.createSObject(theObject, msg.payload);
-            const payload = { sobject: sobj };
-            nforce.extractHeaders(payload, msg);
+          return dmlResult;
+        })
+        .then((results) => {
+          msg.payload = {
+            success: true,
+            object: theObject.toLowerCase(),
+            action: theAction,
+            id: results.id ? results.id : msg.externalId ? msg.externalId : msg.payload.id
+          };
 
-            // Auth and run DML
-            nforce
-                .authenticate(orgResult.org, orgResult.config)
-                .then(oauth => {
-                    let dmlResult;
-                    const org = orgResult.org;
-                    switch (theAction) {
-                        case 'insert':
-                            dmlResult = org.insert(payload);
-                            break;
-                        case 'update':
-                            dmlResult = org.update(payload);
-                            break;
-                        case 'upsert':
-                            if (msg.hasOwnProperty("externalId")) {
-                                sobj.sobject.setExternalId(msg.externalId.field, msg.externalId.value);
-                            }
-                            dmlResult = org.upsert(payload);
-                            break;
-                        case 'delete':
-                            dmlResult = org.delete(payload);
-                            break;
-                        default:
-                            const err = new Error('Unknown method:' + theAction);
-                            nforce.error(node, msg, err);
-                    }
-
-                    return dmlResult;
-                }).then(results => {
-                    msg.payload = {
-                        success: true,
-                        object: theObject.toLowerCase(),
-                        action: theAction,
-                        id: (results.id) ? results.id
-                            : (msg.externalId) ? msg.externalId
-                                : msg.payload.id
-                    }
-
-                    node.send(msg);
-                    node.status({});
-                })
-                .catch(err => nforce.error(node, msg, err));
-        });
-    }
-    RED.nodes.registerType("dml", Dml);
+          node.send(msg);
+          node.status({});
+        })
+        .catch((err) => nforce.error(node, msg, err));
+    });
+  }
+  RED.nodes.registerType('dml', Dml);
 };
--- nforce_wrapper.js
+++ nforce_wrapper.js
@@ -1,39 +1,40 @@
 const nforce8 = require('nforce8');
 
-const createConnection = function (configOptionsRaw, msg) {
-    // Update configOptions from msg if there's incoming credentials
-    const configOptions = getConfig(configOptionsRaw, msg);
-    msg = msg || {};
-    const orgOptions = {
-        mode: 'single',
-        redirectUri: configOptions.callbackUrl,
-        environment: configOptions.environment
-    };
+const createConnection = function(configOptionsRaw, msg) {
+  // Update configOptions from msg if there's incoming credentials
+  const configOptions = getConfig(configOptionsRaw, msg);
+  msg = msg || {};
+  const orgOptions = {
+    /* TODO: remove single option to facilitate alternative logins */
+    mode: 'single',
+    redirectUri: configOptions.callbackUrl,
+    environment: configOptions.environment
+  };
 
-    // Overwrite the endpoints eventually - access instance directly
-    if (configOptions.usePotUrl) {
-        orgOptions.authEndpoint = configOptions.poturl;
-        orgOptions.testAuthEndpoint = configOptions.poturl;
-    }
+  // Overwrite the endpoints eventually - access instance directly
+  if (configOptions.usePotUrl) {
+    orgOptions.authEndpoint = configOptions.poturl;
+    orgOptions.testAuthEndpoint = configOptions.poturl;
+  }
 
-    if (configOptions.apiversion) {
-        orgOptions.apiVersion = configOptions.apiversion;
-    }
+  if (configOptions.apiversion) {
+    orgOptions.apiVersion = configOptions.apiversion;
+  }
 
-    const connectionResult = nforce8.createConnection(orgOptions);
-    const result = {
-        connection: connectionResult,
-        config: configOptions
-    };
+  const connectionResult = nforce8.createConnection(orgOptions);
+  const result = {
+    connection: connectionResult,
+    config: configOptions
+  };
 
-    return result;
+  return result;
 };
 
 // wrapper around the org.authenticate function
 // TODO: Token authentication for Salesforce roundtrip
-const authenticate = function (org, configOptions) {
-    // TODO: Check if we have a incomign session
-    return org.authenticate(configOptions);
+const authenticate = function(org, configOptions) {
+  // TODO: Check if we have a incomign session
+  return org.authenticate(configOptions);
 };
 
 /**
@@ -41,51 +42,50 @@ const authenticate = function (org, configOptions) {
  * @param {*} configOptions the current configuration settings
  * @param {*} msg the actual incoming message that might contain identity information
  */
-const getConfig = function (configOptions, msg) {
-    configOptions = configOptions || {};
-    const connectionOptionResult = Object.assign({}, configOptions);
-    //  Check if  credentials from message overwrite credentials from config
-    if (configOptions.allowMsgCredentials && msg.hasOwnProperty("sf")) {
-        //TODO: Do we really need to check for empty configuration values
-        // or is it OK to overwrite when sf values are present?
-        if (msg.sf.consumerKey && this.connection.consumerKey === '') {
-            connectionOptionResult.consumerKey = msg.sf.consumerKey;
-        }
-        if (msg.sf.consumerSecret && this.connection.consumerSecret === '') {
-            connectionOptionResult.consumerSecret = msg.sf.consumerSecret;
-        }
-        if (msg.sf.username && this.connection.username === '') {
-            connectionOptionResult.username = msg.sf.username;
-        }
-        if (msg.sf.password && this.connection.password === '') {
-            connectionOptionResult.password = msg.sf.password;
-        }
+const getConfig = function(configOptions, msg) {
+  configOptions = configOptions || {};
+  const connectionOptionResult = Object.assign({}, configOptions);
+  //  Check if  credentials from message overwrite credentials from config
+  if (configOptions.allowMsgCredentials && msg.hasOwnProperty('sf')) {
+    //TODO: Do we really need to check for empty configuration values
+    // or is it OK to overwrite when sf values are present?
+    if (msg.sf.consumerKey && this.connection.consumerKey === '') {
+      connectionOptionResult.consumerKey = msg.sf.consumerKey;
     }
+    if (msg.sf.consumerSecret && this.connection.consumerSecret === '') {
+      connectionOptionResult.consumerSecret = msg.sf.consumerSecret;
+    }
+    if (msg.sf.username && this.connection.username === '') {
+      connectionOptionResult.username = msg.sf.username;
+    }
+    if (msg.sf.password && this.connection.password === '') {
+      connectionOptionResult.password = msg.sf.password;
+    }
+  }
 
-    return connectionOptionResult;
-}
+  return connectionOptionResult;
+};
 
-const redError = function (redNode, msg, err) {
-    redNode.status({ fill: 'red', shape: 'dot', text: 'Error:' + err.message });
-    redNode.error(err, msg);
-}
+const redError = function(redNode, msg, err) {
+  redNode.status({ fill: 'red', shape: 'dot', text: 'Error:' + err.message });
+  redNode.error(err, msg);
+};
 
 /**
  * Checks for Salesforce headers in msg object to send back to SF
  * @param {*} payload - the data to be posted
  * @param {*} msg - the incoming message
  */
-const extractHeaders = function (payload, msg) {
-    if (payload && msg && msg.sf && msg.sf.headers) {
-        payload.headers = msg.sf.headers;
-    }
-
-}
+const extractHeaders = function(payload, msg) {
+  if (payload && msg && msg.sf && msg.sf.headers) {
+    payload.headers = msg.sf.headers;
+  }
+};
 
 module.exports = {
-    createConnection: createConnection,
-    authenticate: authenticate,
-    force: nforce8,
-    error: redError,
-    extractHeaders: extractHeaders
-}
+  createConnection: createConnection,
+  authenticate: authenticate,
+  force: nforce8,
+  error: redError,
+  extractHeaders: extractHeaders
+};
